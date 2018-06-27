c89bc4d5428c89100b3c9d4b26328fd7e5b11b0d
russellpbrown@gmail.com <russellpbrown@gmail.com>
2018-06-27 07:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
   cart.splice(0)
-  return `Your total cost is ${total()} which will be charged to the card ${cardNumber}.`
+  return `Your total cost is ${totalPrice} which will be charged to the card ${cardNumber}.`
   }
   
 }
